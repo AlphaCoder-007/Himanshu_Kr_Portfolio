5f91a061dd1173afdba7a7a5f1780d65f270a85a
Your Name <hkt001rpb@gmail.com>
2026-08-23 16:25:19 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Himanshu_Kr_Portfolio/',
   plugins: [react()],
   test: {
     globals: true,
